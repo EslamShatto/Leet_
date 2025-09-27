fbb5ef4af05d244e56a0e3d1a4601c123cf13e98
EslamShatto <eslamshattou@gmail.com>
2025-09-27 21:58:19 +0300

    Add files via upload

---
+++ leetcode/integer-to-roman.py
@@ -0,0 +1,35 @@
+#https://leetcode.com/problems/integer-to-roman/description/
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        int_map = {
+            1000: 'M',
+            900: 'CM',
+            500: 'D',
+            400: 'CD',
+            100: 'C',
+            90: 'XC',
+            50: 'L',
+            40: 'XL',
+            10: 'X',
+            9: 'IX',
+            5: 'V',
+            4: 'IV',
+            1: 'I'
+        }
+        answer=''
+        keys = list(int_map.keys())
+        cnt = 0
+        while num:
+            if num >= keys[cnt]:
+
+                answer += (num // keys[cnt]) * int_map[keys[cnt]]
+                num = num - (num // keys[cnt]) * keys[cnt]
+            cnt +=1
+        return answer
+
+
+
+
+s = Solution()
+print(s.intToRoman(3749))
